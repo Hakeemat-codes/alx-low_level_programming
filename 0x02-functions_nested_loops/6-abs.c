9ad88ca1a86b6c36287565beac80b0ccb00d83fc
Hakeemat-codes <ahakeemat@gmail.com>
2023-10-11 16:11:14 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _abs - computes the absolute value of an integer
+ *  @c: The numberr to be computed
+ *  Return: absolute valuue of number or 0
+ */
+
+int _abs(int c)
+
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+
+
+}
+
