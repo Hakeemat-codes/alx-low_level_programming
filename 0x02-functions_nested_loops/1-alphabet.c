b6725321f0b84895fa953e73dfe349cd977783e0
Hakeemat-codes <ahakeemat@gmail.com>
2023-10-11 09:42:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the main function
+ * Return: return to 0
+ */
+
+void print_alphabet(void)
+{
+
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
