864ab84908e710b127dca3aad6830cd0f6a53776
Hakeemat-codes <ahakeemat@gmail.com>
2023-10-12 15:26:47 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,13 +22,13 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d", n);
+	printf("%d, ", n);
 	}
 	}
 	}
 	else
 	{
-	for (; n >- 98; n--)
+	for (; n >= 98; n--)
 	{
 	if  (n == 98)
 	{
@@ -38,7 +38,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d", n);
+	printf("%d, ", n);
 	}
 	}
 	}
